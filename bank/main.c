a9ebc1673dcfb36249c4c9573b967cb07d8b5888
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-09-13 02:02:06 +0300

    Create a new project

---
+++ bank/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tCustomer {
+    size_t id;
+    char firstName[64];
+    char lastName[64];
+};
+
+struct tAccount {
+    size_t number;
+    double balance;
+};
+
+int main()
+{
+    return 0;
+}
