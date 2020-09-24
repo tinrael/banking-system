c3f176e2aa9ae4aac0d3cdbf9cdcf95f06f63397
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-09-24 18:01:32 +0300

    Implemented the eraseFromList and deleteListNode functions

--- bank/DoublyLinkedList.h
+++ bank/DoublyLinkedList.h
@@ -5,16 +5,16 @@
 #include <stdio.h>
 
 typedef struct tagNode {
-    int data;
+    int key;
 
     struct tagNode* next;
     struct tagNode* prev;
 } tNode;
 
 // inserts an element to the beginning
-void add(tNode** head, int data) {
+void addToList(tNode** head, int key) {
     tNode* newOne = (tNode*) malloc(sizeof(tNode));
-    newOne->data = data;
+    newOne->key = key;
 
     newOne->next = *head;
     newOne->prev = NULL;
@@ -26,11 +26,43 @@ void add(tNode** head, int data) {
     (*head) = newOne;
 }
 
-void print(tNode* head) {
+// erases the list node pointed at by toDelete
+void deleteListNode(tNode** head, tNode* toDelete) {
+    if ((*head) != NULL && toDelete != NULL) {
+        if ((*head) == toDelete) {
+            (*head) = toDelete->next;
+        }
+
+        if (toDelete->next != NULL) {
+            toDelete->next->prev = toDelete->prev;
+        }
+
+        if (toDelete->prev != NULL) {
+            toDelete->prev->next = toDelete->next;
+        }
+
+        free(toDelete);
+    }
+}
+
+// erases the element with the key 'key'
+void eraseFromList(tNode** head, int key) {
+    tNode* current = *head;
+    while (current != NULL) {
+        if (current->key == key) {
+            deleteListNode(head, current);
+            return;
+        }
+        current = current->next;
+    }
+}
+
+void printList(tNode* head) {
     while (head != NULL) {
-        printf("%d -> ", head->data);
+        printf("%d -> ", head->key);
         head = head->next;
     }
+    printf("\n");
 }
 
 #endif // DOUBLYLINKEDLIST_H_INCLUDED
